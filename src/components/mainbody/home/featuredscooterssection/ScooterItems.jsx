aef494013fda46f644b8d828ff38bcf3b779f412
LipoStyle <lipostylee@gmail.com>
2026-02-16 11:16:05 +0200

    adding 2 vehicles and updating some visuals

--- src/components/mainbody/home/featuredscooterssection/ScooterItems.jsx
+++ src/components/mainbody/home/featuredscooterssection/ScooterItems.jsx
@@ -1,39 +1,51 @@
-import cf450 from "../../../../images/home/ATV 450.jpg";
-import cf450_long from "../../../../images/home/ATV 450 LONG.jpg";
-import cf520_long from "../../../../images/home/atv520.jpeg";
-import sym50 from "../../../../images/home/sym50cc.jpg";
-import sym125 from "../../../../images/home/SYM 125.jpg";
-import sym200 from "../../../../images/home/sym200.jpg";
+import cf450 from "../../../../images/home/ATV_450.png";
+import cf450_long from "../../../../images/home/ATV_450_LONG.png";
+import cf520_long from "../../../../images/home/atv520.png";
+import sym50 from "../../../../images/home/sym50cc.png";
+import sym125 from "../../../../images/home/SYM_125.png";
+import sym200 from "../../../../images/home/sym200.png";
+import kymco_310 from "../../../../images/home/atv_310cc_kymco.png";
+import skoda_fabia_1000 from "../../../../images/home/skoda-fabia-1000.png";
 
 export const scooterItems = [
   {
     id: 1,
     img: sym50,
-    title: "Sym sr 50cc"
+    title: "Sym sr 50cc",
   },
   {
     id: 2,
     img: sym125,
-    title: "Sym sr 125cc"
+    title: "Sym sr 125cc",
   },
   {
     id: 3,
     img: sym200,
-    title: "Sym st 200cc"
+    title: "Sym st 200cc",
   },
   {
     id: 4,
     img: cf450,
-    title: "Cforce atv 450cc"
+    title: "Cforce atv 450cc",
   },
   {
     id: 5,
     img: cf450_long,
-    title: "Cforce atv 450cc long"
+    title: "Cforce atv 450cc long",
   },
   {
     id: 6,
     img: cf520_long,
-    title: "Cforce atv 520cc long"
-  }
-]
+    title: "Cforce atv 520cc long",
+  },
+  {
+    id: 7,
+    img: kymco_310,
+    title: "Kymco atv 310cc",
+  },
+  {
+    id: 8,
+    img: skoda_fabia_1000,
+    title: "Skoda Fabia 1000cc",
+  },
+];
--- src/components/mainbody/scooter-rental/scooterContent.jsx
+++ src/components/mainbody/scooter-rental/scooterContent.jsx
@@ -1,12 +1,13 @@
-import cf450 from "../../../images/home/ATV 450.jpg";
-import cf450_long from "../../../images/home/ATV 450 LONG.jpg";
-import cf520_long from "../../../images/home/atv520.jpeg";
-import sym50 from "../../../images/home/sym50cc.jpg";
-import sym125 from "../../../images/home/SYM 125.jpg";
-import sym200 from "../../../images/home/sym200.jpg";
+import cf450 from "../../../images/home/ATV_450.png";
+import cf450_long from "../../../images/home/ATV_450_LONG.png";
+import cf520_long from "../../../images/home/atv520.png";
+import sym50 from "../../../images/home/sym50cc.png";
+import sym125 from "../../../images/home/SYM_125.png";
+import sym200 from "../../../images/home/sym200.png";
+import kymco_310 from "../../../images/home/atv_310cc_kymco.png";
+import skoda_fabia_1000 from "../../../images/home/skoda-fabia-1000.png";
 
 const scooterContent = [
-
   {
     title: "Sym sr 50cc",
     images: sym50,
@@ -30,7 +31,15 @@ const scooterContent = [
   {
     title: "Cforce atv 520cc long",
     images: cf520_long,
-  }
+  },
+  {
+    title: "Kymco atv 310cc",
+    images: cf520_long,
+  },
+  {
+    title: "Skoda Fabia 1000cc",
+    images: skoda_fabia_1000,
+  },
 ];
 
 export default scooterContent;
